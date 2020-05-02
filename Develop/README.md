05c8fdaa161291f5fc3486a2d19e720e22e4b08b
surajgurung <microbiosuraj@gmail.com>
2020-05-02 18:30:05 -0400

    files were push

--- Develop/README.md
+++ Develop/README.md
@@ -1,9 +1,47 @@
-#Good README Generator 
+# Good README Generator 
  
 ![Avatar](https://avatars3.githubusercontent.com/u/60562938?v=4)
  
-Your User Name : surajbgurung
+Your usrname is: surajbgurung
  
 Your mail is: null
 
-##Good Readme Body 
+
+![bage image](https://img.shields.io/static/v1?label=Version&message=dsfsd&color=<COLOR>) 
+# sdfds 
+ 
+## Description: 
+
+ dfsfsd 
+ 
+## Table of contents
+ 
+### Installation
+
+ dsfdsf
+ 
+
+### Usage
+ 
+ dsffdsf
+ 
+### Credits 
+ 
+ dsfds
+ 
+### License 
+ 
+ dsfasdf
+ 
+## Contributing 
+ 
+ fdsfsd
+ 
+## Tests 
+ 
+ fdsadsf
+ 
+## Questions 
+ 
+ adsfadsf
+ 
--- Develop/index.js
+++ Develop/index.js
@@ -1,111 +1,208 @@
+
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-// // const questions = [{
-//     message:"what is your user name?",
-//     name:"username",
-//     type:"input"
-// },
-//     {
-//         message:"What is a badge name? ",
-//         name: "badge",
-//         type:"input"
-//     }
-//     // {
-//     //     message:"What is the project title? ",
-//     //     name: "project",
-//     //     type:"input"
-//     // },
-//     // {
-//     //     message:"What is the description? ",
-//     //     name: "description",
-//     //     type:"input"
-//     // },
-//     // {
-//     //     message:"What are the table of contents? ",
-//     //     name: "table",
-//     //     type:"input"
-//     // },
-//     // {
-//     //     message:"What is the installation? ",
-//     //     name: "install",
-//     //     type:"input"
-//     // },
-//     // {
-//     //     message:"What is the usage? ",
-//     //     name: "usage",
-//     //     type:"input"
-//     // },
-//     // {
-//     //     message:"What is the license number? ",
-//     //     name: "license",
-//     //     type:"input"
-//     // },
-//     // ,
-//     // {
-//     //     message:"Who is contributing? ",
-//     //     name: "contribute",
-//     //     type:"input"
-//     // },
-//     // {
-//     //     message:"What are the required tests? ",
-//     //     name: "test",
-//     //     type:"input"
-//     ];
+
+// const questions = [
+// 	// {
+// 	// 	message: "Enter your GitHub username:",
+// 	// 	name: "username",
+// 	// 	type:"input"
+// 	// },
+// 	{
+// 		message:"What is a badge name? ",
+// 		name: "badge",
+// 		type:"input"
+// 	},
+// 	{
+// 		message:"What is the project title? ",
+// 		name: "project",
+// 		type:"input"
+// 	},
+// 	{
+// 		message:"What is the description? ",
+// 		name: "description",
+// 		type:"input"
+// 	},
+// 	{
+// 		message:"What are the table of contents? ",
+// 		name: "table",
+// 		type:"input"
+// 	},
+// 	{
+// 		message:"What is the installation? ",
+// 		name: "install",
+// 		type:"input"
+// 	},
+// 	{
+// 		message:"What is the usage? ",
+// 		name: "usage",
+// 		type:"input"
+// 	},
+// 	{
+// 		message:"What is the license number? ",
+// 		name: "license",
+// 		type:"input"
+// 	},
+// 	,
+// 	{
+// 		message:"Who is contributing? ",
+// 		name: "contribute",
+// 		type:"input"
+// 	},
+// 	{
+// 		message:"What are the required tests? ",
+// 		name: "test",
+// 		type:"input"
+// 	}
+// ];
 
 
 inquirer
-    .prompt({
-            message:"what is your user name?",
-           name:"username",
-           type:"input"
-        },
-        
-        
-    )
-    .then(function () {
-        
-        //const queryUrl = `https://api.github.com/search/users?q=${username}in%3Ausername`;
-        const queryUrl = `  https://api.github.com/users/${username}`;
-        axios.get(queryUrl).then(function (res) {
-            console.log(res);
-            const userName = res.data.login;
-            const avatarUrl = res.data.avatar_url;
-            const avatar = '![Avatar](' + avatarUrl + ')';
-            //get email
-            const email = res.data.email;
-            //for heaeer
-            let header = '#Good README Generator \n \n' +
-                avatar + '\n \n' +
-                'Your User Name : ' + userName + '\n \n' +
-                'Your mail is: ' + email + '\n';
-            let body = '##Good Readme Body \n';
-            var document = header.concat('\n', body);
-            fs.writeFile("README.md", document, function (err) {
-                if (err) {
-                    throw err;
+	.prompt({
+			message: "Enter your GitHub username:",
+			name: "username",
+			type:"input",
+	})
+	.then(function ({ username }) {
+		const queryUrl = `https://api.github.com/users/${username}`;
+
+		axios.get(queryUrl).then(function (res) {
+			// console.log(res.data);
+
+			// Get Username
+			const username = res.data.login;
+
+			// Get Avatar Image
+			const avatarURL = res.data.avatar_url;
+			const avatar = '![Avatar](' + avatarURL + ')';
+
+			// Get Email
+			const email = res.data.email;
+
+			let document = 
+			// Avatar
+			' \n \n' +
+			avatar + '\n \n' +
+			// Usermame
+			'Your usrname is: ' + username + '\n \n' +
+			// Email
+			'Your mail is: ' + email + '\n';
+
+			fs.writeFile("README.md", document, function (err) {
+				if (err) {
+					throw err;
+				}
+			});
+		})
+
+		.then(function(){
+			inquirer.prompt([
+				{
+					message:"What is your badge version? ",
+					name: "badge",
+					type:"input"
+				},
+				{
+					message:"What is the project title? ",
+					name: "project",
+					type:"input"
+				},
+				{
+					message:"What is your project description? ",
+					name: "description",
+					type:"input"
+				},
+				{
+					message:"Table of contents are as follows(press enter)? ",
+					name: "table",
+					//type:"input"
+				},
+				{
+					message:"installation? ",
+					name: "install",
+					type:"input"
+                },
+                {
+					message:"usages ? ",
+					name: "usage",
+					type:"input"
+				},
+                
+                {
+					message:"credits ? ",
+					name: "credit",
+					type:"input"
+				},
+				 {
+					message:" license ? ",
+					name: "license",
+					type:"input"
+				},
+				
+				{
+					message:"Who is contributing? ",
+					name: "contribute",
+					type:"input"
+				},
+				{
+					message:"What are the required tests? ",
+					name: "test",
+					type:"input"
+                },
+                {
+                    message:"what is the questions?",
+                    name:"questions",
+                    type:"input"
                 }
-            })
+			])
+			.then(function(answers){
+				console.log(answers);
+
+				//let userQs = 
+				// Prompt second set of questions
+				//`## Second Set of Questions  \n \n`
+	
+				let body2 =
+
+				// Badge
+				`\n\n![bage image](https://img.shields.io/static/v1?label=Version&message=${answers.badge}&color=<COLOR>) \n`+
+
+				// Project Title
+				`# ${answers.project} \n \n`+
+
+				// Project Description
+                `## Description: \n\n ${answers.description} \n \n`+
+                //table of content
+                `## Table of contents\n \n`+//${answers.table}\n \n`+
+                //installation
+                `### Installation\n\n ${answers.install}\n \n\n`+
+                //usage
+                `### Usage\n \n ${answers.usage}\n \n`+
+                //Credits
+                `### Credits \n \n ${answers.credit}\n \n`+
+                 //License
+                `### License \n \n ${answers.license}\n \n`+
+                //contributing
+                `## Contributing \n \n ${answers.contribute}\n \n`+
+                //tests
+                `## Tests \n \n ${answers.test}\n \n`+
+                //questions
+                `## Questions \n \n ${answers.questions}`
+                
+                
+                
 
 
-        });
-    });
+
+				var document =  body2;
+				fs.appendFile("README.md", document, function (err) {
+					if (err) {
+						throw err;
+					}
+				});
+			})
+		})
+	})
 
 
-
-
-
-
-
-// const questions = [
-
-// ];
-
-// function writeToFile(fileName, data) {
-// }
-
-// function init() {
-
-// }
-
-// init();
